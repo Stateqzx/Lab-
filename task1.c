78a5b60383a5b14d7bd7f9bd0512dab9d824915a
Stateqzx <72642307+Stateqzx@users.noreply.github.com>
2020-10-10 16:49:31 +0700

    Update task1.c

--- task1.c
+++ task1.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-  for (int i=1; i<=100; i++)
+  for (int i = 1; i <= 100; i++)
   {
-    if (i%15==0)
+    if (i % 15 == 0)
     {
       printf("FizzBuzz\n");
     }
-    else if(i%3==0 && i%5 !=0)
+    else if(i % 3 == 0 && i % 5 != 0)
     {
       printf("Fizz\n");
     }
-    else if(i%5==0 && i%3 !=0)
+    else if(i % 5 == 0 && i % 3 != 0)
     {
       printf("Buzz\n");
     }
@@ -21,5 +21,6 @@ int main()
       printf("%d\n",i);
     }
   }
-return 0;
+
+  return 0;
 }
