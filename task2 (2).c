ca30d3c1acff1cab5524a906717c6a8994f4116b
Stateqzx <72642307+Stateqzx@users.noreply.github.com>
2020-10-10 15:53:26 +0700

    Add files via upload

---
+++ task2 (2).c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int is_simple(int number)
+{
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main(void)
+{
+    int num;
+    scanf("%d", &num);
+    printf("%d", is_simple(num));
+
+    return 0;
+}
