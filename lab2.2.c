586d178c913d95120bf520c832d60c5f26f4823d
Stateqzx <72642307+Stateqzx@users.noreply.github.com>
2020-10-31 13:23:04 +0700

    Update lab2.2.c

--- lab2.2.c
+++ lab2.2.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 
-long long get_fibbonachi_via_recursion(int number)
-{
-	if (number == 1 || number == 2)
-	{
-		return 1;
-	}
+long long cache[300];
 
-	return get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2);
-}
+long long get_fibbonachi_via_recursion(int number);
 
 int main()
 {
-	int n;
-	printf("number = ");
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
-	{
-		printf("%lld ", get_fibbonachi_via_recursion(i));
-	}
-	getchar(); getchar();
+    int number;
+    printf("Enter n for find fibbonachi: ");
+    scanf("%d", &number);
+    for (int count = 1; count < number; count++)
+    {
+        printf("%lld", get_fibbonachi_via_recursion(count));
+    }
+      
+    return 0;
+}
 
-	return 0;
+long long get_fibbonachi_via_recursion(int number)
+{
+    if (cache[number] == 0)
+    {
+        if (number == 1 || number == 2)
+        {
+            cache[number] = 1;
+        }
+        else
+        {
+            cache[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2);
+        }
+    }
+
+    return cache[number];
 }
